c1e613ceb6f85b35f4fe12c0d00746a95dc973e9
Gabriel Ivăncescu <gabrielopcode@gmail.com>
2024-07-24 20:15:33 +0300

    mshtml: Use a shared propput_dispid global.
    Simplifies when it gets used more instead of having to declare it everytime.
    
    Signed-off-by: Gabriel Ivăncescu <gabrielopcode@gmail.com>

--- dlls/mshtml/mshtml_private.h
+++ dlls/mshtml/mshtml_private.h
@@ -414,6 +414,7 @@ typedef struct {
 } dispex_static_data_t;
 
 extern const tid_t no_iface_tids[1];
+extern DISPID propput_dispid;
 
 typedef HRESULT (*dispex_hook_invoke_t)(DispatchEx*,WORD,DISPPARAMS*,VARIANT*,
                                         EXCEPINFO*,IServiceProvider*);
